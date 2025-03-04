9f00399729c053157eb552100c06dfe03265f3ba
harelog3 <harelolguin@MacBook-Pro-de-Harel.local>
2025-03-03 23:44:12 -0600

    feat(NextJS application setup): Setted up frontend with NextJS app

---
+++ next.config.ts
@@ -0,0 +1,7 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    output: "export", // Export application as static files
+};
+
+export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <button className="btn btn-primary">Generic button</button>
+        </div>
+    );
+}
